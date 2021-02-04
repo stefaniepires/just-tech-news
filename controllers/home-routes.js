35eb89ba35f1dd13eba1db33e806c446ea414e1a
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-02-04 10:48:34 -0600

    used handlebars to create homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      //will loop over and map each Sequelize object 
+      //into a serialized version of itself, saving the 
+      //results in a new posts array
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // add the array to an object and continue passing an object to the template
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
